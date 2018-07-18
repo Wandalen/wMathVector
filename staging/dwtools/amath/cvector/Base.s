6e8b2be95a7ff3b65a54320ecbf14b3c282acc21
Wandalen <wandalen.me@gmail.com>
2018-07-18 10:56:52 +0300

    sync

--- staging/dwtools/amath/cvector/Base.s
+++ staging/dwtools/amath/cvector/Base.s
@@ -1,6 +1,6 @@
 (function _Base_s_() {
 
-'use strict'; /**/
+'use strict';
 
 if( typeof module !== 'undefined' )
 {
--- staging/dwtools/amath/cvector/eRoutines.s
+++ staging/dwtools/amath/cvector/eRoutines.s
@@ -1,6 +1,6 @@
 (function _fVectorRoutines_s_() {
 
-'use strict'; /**/
+'use strict';
 
 var _ = _global_.wTools;
 var _hasLength = _.hasLength;
