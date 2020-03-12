1e8860ff9b7ee6705e231588d5da675c41060f38
Dmytro <dm.vict.kr@gmail.com>
2020-03-12 17:13:37 +0200

    Implement test routine `reviewSrcIsAdapterRoutineFromMaybeNumber`

--- proto/dwtools/amath/l3_vector.test/VectorAdapter.test.s
+++ proto/dwtools/amath/l3_vector.test/VectorAdapter.test.s
@@ -204,33 +204,6 @@ function toLong( test )
 
 //
 
-function fromNumberReview( test )
-{
-
-  test.case = 'basic';
-  var exp = [ 3, 3, 3, 3 ];
-  var v = _.vectorAdapter.fromNumber( 3, 4 );
-  test.equivalent( v, exp );
-  var exp = [ 3, 3 ];
-  var v2 = v.review([ 1, 2 ]);
-  test.equivalent( v2, exp );
-  test.is( !!v2._vectorBuffer );
-  test.is( v2._vectorBuffer !== v._vectorBuffer );
-  var exp = [ 3, 3 ];
-  var v2 = v.review([ 0, 1 ]);
-  test.equivalent( v2, exp );
-  test.is( !!v2._vectorBuffer );
-  test.is( v2._vectorBuffer !== v._vectorBuffer );
-  var exp = [ 3, 3 ];
-  var v2 = v.review([ 2, 3 ]);
-  test.equivalent( v2, exp );
-  test.is( !!v2._vectorBuffer );
-  test.is( v2._vectorBuffer !== v._vectorBuffer );
-
-}
-
-//
-
 function reviewSrcIsSimpleVector( test )
 {
   test.case = 'src - empty vector, crange - 0';
@@ -868,6 +841,231 @@ function reviewSrcIsAdapterRoutineFromNumber( test )
 
   test.case = 'crange[ 0 ] > crange[ 1 ]';
   var src = vad.fromNumber( 5, 6 );
+  var got = vad.review( src, [ 3, 2 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+}
+
+//
+
+function reviewSrcIsAdapterRoutineFromMaybeNumber( test )
+{
+  test.case = 'src - empty vector, crange - 0';
+  var src = vad.fromMaybeNumber( vad.from( [] ), 0 );
+  var got = vad.review( src, 0 );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange - 0';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, 0 );
+  var exp = vad.from( [ 0, 1, 2, 3, 4, 5 ] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange > 0 && crange < src.length - 1';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, 2 );
+  var exp = vad.from( [ 2, 3, 4, 5 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange - src.length';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, 6 );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  /* */
+
+  test.case = 'src - empty vector, crange[ 0 ] and crange[ 1 ] - -1';
+  var src = vad.fromMaybeNumber( vad.from( [] ), 0 );
+  var got = vad.review( src, [ 0, -1 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange[ 0 ] - 0, crange[ 1 ] - src.length';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, [ 0, 5 ] );
+  var exp = vad.from( [ 0, 1, 2, 3, 4, 5 ] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange[ 0 ] - 0, crange < src.length';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, [ 0, 3 ] );
+  var exp = vad.from( [ 0, 1, 2, 3 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] > 0, crange < src.length';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, [ 1, 3 ] );
+  var exp = vad.from( [ 1, 2, 3 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] and crange[ 1 ] - src.length';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, [ 6, 5 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] > crange[ 1 ]';
+  var src = vad.fromMaybeNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
+  var got = vad.review( src, [ 3, 2 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  /* */
+
+  test.case = 'src - empty vector, crange - 0';
+  var src = vad.fromMaybeNumber( [], 0 );
+  var got = vad.review( src, 0 );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange - 0';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, 0 );
+  var exp = vad.from( [ 0, 1, 2, 3, 4, 5 ] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange > 0 && crange < src.length - 1';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, 2 );
+  var exp = vad.from( [ 2, 3, 4, 5 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange - src.length';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, 6 );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  /* */
+
+  test.case = 'src - empty vector, crange[ 0 ] and crange[ 1 ] - -1';
+  var src = vad.fromMaybeNumber( [], 0 );
+  var got = vad.review( src, [ 0, -1 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange[ 0 ] - 0, crange[ 1 ] - src.length';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, [ 0, 5 ] );
+  var exp = vad.from( [ 0, 1, 2, 3, 4, 5 ] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange[ 0 ] - 0, crange < src.length';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, [ 0, 3 ] );
+  var exp = vad.from( [ 0, 1, 2, 3 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] > 0, crange < src.length';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, [ 1, 3 ] );
+  var exp = vad.from( [ 1, 2, 3 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] and crange[ 1 ] - src.length';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, [ 6, 5 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] > crange[ 1 ]';
+  var src = vad.fromMaybeNumber( [ 0, 1, 2, 3, 4, 5 ], 6 );
+  var got = vad.review( src, [ 3, 2 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  /* */
+
+  test.case = 'src - empty vector, crange - 0';
+  var src = vad.fromMaybeNumber( 5, 0 );
+  var got = vad.review( src, 0 );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange - 0';
+  var src = vad.fromMaybeNumber( 5, 6 );
+  var got = vad.review( src, 0 );
+  var exp = vad.from( [ 5, 5, 5, 5, 5, 5 ] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange > 0 && crange < src.length - 1';
+  var src = vad.fromMaybeNumber( 5, 6 );
+  var got = vad.review( src, 2 );
+  var exp = vad.from( [ 5, 5, 5, 5 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange - src.length';
+  var src = vad.fromMaybeNumber( 5, 6 );
+  var got = vad.review( src, 6 );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  /* */
+
+  test.case = 'src - empty vector, crange[ 0 ] and crange[ 1 ] - -1';
+  var src = vad.fromMaybeNumber( 5, 0 );
+  var got = vad.review( src, [ 0, -1 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange[ 0 ] - 0, crange[ 1 ] - src.length';
+  var src = vad.fromMaybeNumber( 5, 6 );
+  var got = vad.review( src, [ 0, 5 ] );
+  var exp = vad.from( [ 5, 5, 5, 5, 5, 5 ] );
+  test.identical( got, exp );
+  test.is( got === src );
+
+  test.case = 'crange[ 0 ] - 0, crange < src.length';
+  var src = vad.fromMaybeNumber( 5, 6 );
+  var got = vad.review( src, [ 0, 3 ] );
+  var exp = vad.from( [ 5, 5, 5, 5 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] > 0, crange < src.length';
+  var src = vad.fromMaybeNumber( 5, 6 );
+  var got = vad.review( src, [ 1, 3 ] );
+  var exp = vad.from( [ 5, 5, 5 ] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] and crange[ 1 ] - src.length';
+  var src = vad.fromMaybeNumber( 5, 6 );
+  var got = vad.review( src, [ 6, 5 ] );
+  var exp = vad.from( [] );
+  test.identical( got, exp );
+  test.is( got !== src );
+
+  test.case = 'crange[ 0 ] > crange[ 1 ]';
+  var src = vad.fromMaybeNumber( 5, 6 );
   var got = vad.review( src, [ 3, 2 ] );
   var exp = vad.from( [] );
   test.identical( got, exp );
@@ -2615,8 +2813,6 @@ var Self =
 
     // 
 
-    fromNumberReview,
-
     reviewSrcIsSimpleVector,
     reviewSrcIsAdapterRoutineFrom,
     reviewSrcIsAdapterRoutineFromLong,
@@ -2624,6 +2820,7 @@ var Self =
     reviewSrcIsAdapterRoutineFromLongLrange,
     reviewSrcIsAdapterRoutineFromLongLrangeAndStride,
     reviewSrcIsAdapterRoutineFromNumber,
+    reviewSrcIsAdapterRoutineFromMaybeNumber,
 
     // iterator
 
