2d9f0711f292d3f7c2e1d8ec462b67920affd0cd
Dmytro <dm.vict.kr@gmail.com>
2020-03-13 12:31:01 +0200

    Extend test routine `reviewSrcIsAdapterRoutineFromNumber`, namespace 'avector'

--- proto/dwtools/amath/l3_vector.test/VectorLong.test.s
+++ proto/dwtools/amath/l3_vector.test/VectorLong.test.s
@@ -740,77 +740,96 @@ function reviewSrcIsAdapterRoutineFromLongLrangeAndStride( test )
 
 function reviewSrcIsAdapterRoutineFromNumber( test )
 {
-  test.case = 'src - empty vector, crange - 0';
-  var src = vad.fromNumber( vad.from( [] ), 0 );
-  var got = _.avector.review( src, 0 );
-  var exp = [];
-  test.identical( got, exp );
-  test.is( got !== src );
+  var list = 
+  [
+    _.arrayMake,
+    I16x,
+    F32x
+  ];
 
-  test.case = 'crange - 0';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, 0 );
-  var exp = [ 0, 1, 2, 3, 4, 5 ];
-  test.identical( got, exp );
-  test.is( got !== src );
+  for( let i = 0 ; i < list.length ; i++ )
+  {
+    test.open( `long - ${ list[ i ].name }` );
+    testRun( list[ i ] );
+    test.close( `long - ${ list[ i ].name }` );
+  }
 
-  test.case = 'crange > 0 && crange < src.length - 1';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, 2 );
-  var exp = [ 2, 3, 4, 5 ];
-  test.identical( got, exp );
-  test.is( got !== src );
+  /* - */
 
-  test.case = 'crange - src.length';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, 6 );
-  var exp = [];
-  test.identical( got, exp );
-  test.is( got !== src );
+  function testRun( makeLong )
+  {
+    test.case = 'src - empty vector, crange - 0';
+    var src = vad.fromNumber( vad.from( new makeLong( [] ) ), 0 );
+    var got = _.avector.review( src, 0 );
+    var exp = new makeLong( [] );
+    test.identical( got, exp );
+    test.is( got !== src );
 
-  /* */
+    test.case = 'crange - 0';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, 0 );
+    var exp = new makeLong( [ 0, 1, 2, 3, 4, 5 ] );
+    test.identical( got, exp );
+    test.is( got !== src );
 
-  test.case = 'src - empty vector, crange[ 0 ] and crange[ 1 ] - -1';
-  var src = vad.fromNumber( vad.from( [] ), 0 );
-  var got = _.avector.review( src, [ 0, -1 ] );
-  var exp = [];
-  test.identical( got, exp );
-  test.is( got !== src );
+    test.case = 'crange > 0 && crange < src.length - 1';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, 2 );
+    var exp = new makeLong( [ 2, 3, 4, 5 ] );
+    test.identical( got, exp );
+    test.is( got !== src );
 
-  test.case = 'crange[ 0 ] - 0, crange[ 1 ] - src.length';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, [ 0, 5 ] );
-  var exp = [ 0, 1, 2, 3, 4, 5 ];
-  test.identical( got, exp );
-  test.is( got !== src );
+    test.case = 'crange - src.length';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, 6 );
+    var exp = new makeLong( [] );
+    test.identical( got, exp );
+    test.is( got !== src );
 
-  test.case = 'crange[ 0 ] - 0, crange < src.length';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, [ 0, 3 ] );
-  var exp = [ 0, 1, 2, 3 ];
-  test.identical( got, exp );
-  test.is( got !== src );
+    /* */
 
-  test.case = 'crange[ 0 ] > 0, crange < src.length';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, [ 1, 3 ] );
-  var exp = [ 1, 2, 3 ];
-  test.identical( got, exp );
-  test.is( got !== src );
+    test.case = 'src - empty vector, crange[ 0 ] and crange[ 1 ] - -1';
+    var src = vad.fromNumber( vad.from( new makeLong( [] ) ), 0 );
+    var got = _.avector.review( src, [ 0, -1 ] );
+    var exp = new makeLong( [] );
+    test.identical( got, exp );
+    test.is( got !== src );
 
-  test.case = 'crange[ 0 ] and crange[ 1 ] - src.length';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, [ 6, 5 ] );
-  var exp = [];
-  test.identical( got, exp );
-  test.is( got !== src );
+    test.case = 'crange[ 0 ] - 0, crange[ 1 ] - src.length';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, [ 0, 5 ] );
+    var exp = new makeLong( [ 0, 1, 2, 3, 4, 5 ] );
+    test.identical( got, exp );
+    test.is( got !== src );
 
-  test.case = 'crange[ 0 ] > crange[ 1 ]';
-  var src = vad.fromNumber( vad.from( [ 0, 1, 2, 3, 4, 5 ] ), 6 );
-  var got = _.avector.review( src, [ 3, 2 ] );
-  var exp = [];
-  test.identical( got, exp );
-  test.is( got !== src );
+    test.case = 'crange[ 0 ] - 0, crange < src.length';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, [ 0, 3 ] );
+    var exp = new makeLong( [ 0, 1, 2, 3 ] );
+    test.identical( got, exp );
+    test.is( got !== src );
+
+    test.case = 'crange[ 0 ] > 0, crange < src.length';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, [ 1, 3 ] );
+    var exp = new makeLong( [ 1, 2, 3 ] );
+    test.identical( got, exp );
+    test.is( got !== src );
+
+    test.case = 'crange[ 0 ] and crange[ 1 ] - src.length';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, [ 6, 5 ] );
+    var exp = new makeLong( [] );
+    test.identical( got, exp );
+    test.is( got !== src );
+
+    test.case = 'crange[ 0 ] > crange[ 1 ]';
+    var src = vad.fromNumber( vad.from( new makeLong( [ 0, 1, 2, 3, 4, 5 ] ) ), 6 );
+    var got = _.avector.review( src, [ 3, 2 ] );
+    var exp = new makeLong( [] );
+    test.identical( got, exp );
+    test.is( got !== src );
+  }
 
   /* */
 
