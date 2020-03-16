3835b92b6408f8401e60eebd9436364cb096a637
Dmytro <dm.vict.kr@gmail.com>
2020-03-16 17:17:22 +0200

    Implement test routine `allRoutineFromNumberWithNumber`, namespace `avector`

--- proto/dwtools/amath/l3_vector.test/VectorLong.test.s
+++ proto/dwtools/amath/l3_vector.test/VectorLong.test.s
@@ -10069,6 +10069,147 @@ function allRoutineFromNumberWithVectorAdapter( test )
 
 //
 
+function allRoutineFromNumberWithNumber( test )
+{
+  var list = 
+  [
+    _.arrayMake,
+    I16x,
+    F32x
+  ];
+
+  for( let i = 0 ; i < list.length ; i++ )
+  {
+    test.open( `long - ${ list[ i ].name }` );
+    testRun( list[ i ] );
+    test.close( `long - ${ list[ i ].name }` );
+  }
+
+  /* - */
+
+  function testRun( makeLong )
+  {
+    test.case = 'src - empty vector, without onEach';
+    var src = _.vectorAdapter.fromNumber( 7, 0 );
+    var got = _.vectorAdapter.all( src );
+    test.identical( got, true );
+
+    test.case = 'src - vector without zeros, without onEach';
+    var src = _.vectorAdapter.fromNumber( 7, 6 );
+    var got = _.vectorAdapter.all( src );
+    test.identical( got, true );
+
+    test.case = 'src - vector with zeros, without onEach';
+    var src = _.vectorAdapter.fromNumber( 0, 6 );
+    var got = _.vectorAdapter.all( src );
+    test.identical( got, 0 );
+
+    /* */
+
+    test.case = 'src - empty vector, onEach - null';
+    var src = _.vectorAdapter.fromNumber( 7, 0 );
+    var got = _.vectorAdapter.all( src, null );
+    test.identical( got, true );
+
+    test.case = 'src - vector without zeros, onEach - null';
+    var src = _.vectorAdapter.fromNumber( 7, 6 );
+    var got = _.vectorAdapter.all( src, null );
+    test.identical( got, true );
+
+    test.case = 'src - vector with zeros, onEach - null';
+    var src = _.vectorAdapter.fromNumber( 0, 6 );
+    var got = _.vectorAdapter.all( src, null );
+    test.identical( got, 0 );
+
+    /* */
+
+    test.case = 'src - empty vector, onEach - undefined';
+    var src = _.vectorAdapter.fromNumber( 7, 0 );
+    var got = _.vectorAdapter.all( src, undefined );
+    test.identical( got, true );
+
+    test.case = 'src - vector without zeros, onEach - undefined';
+    var src = _.vectorAdapter.fromNumber( 7, 6 );
+    var got = _.vectorAdapter.all( src, undefined );
+    test.identical( got, true );
+
+    test.case = 'src - vector with zeros, onEach - undefined';
+    var src = _.vectorAdapter.fromNumber( 0, 6 );
+    var got = _.vectorAdapter.all( src, undefined );
+    test.identical( got, 0 );
+
+    /* */
+
+    test.case = 'src - empty vector, onEach - returns element';
+    var src = _.vectorAdapter.fromNumber( 7, 0 );
+    var got = _.vectorAdapter.all( src, ( e ) => e );
+    test.identical( got, true );
+
+    test.case = 'src - vector without zeros, onEach - returns element';
+    var src = _.vectorAdapter.fromNumber( 7, 6 );
+    var got = _.vectorAdapter.all( src, ( e ) => e );
+    test.identical( got, true );
+
+    test.case = 'src - vector with zeros, onEach - returns element';
+    var src = _.vectorAdapter.fromNumber( 0, 6 );
+    var got = _.vectorAdapter.all( src, ( e ) => e );
+    test.identical( got, 0 );
+
+    /* */
+
+    test.case = 'src - empty vector, onEach - returns key';
+    var src = _.vectorAdapter.fromNumber( 7, 0 );
+    var got = _.vectorAdapter.all( src, ( e, k ) => k );
+    test.identical( got, true );
+
+    test.case = 'src - vector without zeros, onEach - returns key';
+    var src = _.vectorAdapter.fromNumber( 7, 6 );
+    var got = _.vectorAdapter.all( src, ( e, k ) => k );
+    test.identical( got, 0 );
+
+    test.case = 'src - vector with zeros, onEach - returns key';
+    var src = _.vectorAdapter.fromNumber( 0, 6 );
+    var got = _.vectorAdapter.all( src, ( e, k ) => k );
+    test.identical( got, 0 );
+
+    /* */
+
+    test.case = 'src - empty vector, onEach - returns src.length';
+    var src = _.vectorAdapter.fromNumber( 7, 0 );
+    var got = _.vectorAdapter.all( src, ( e, k, s ) => s.length );
+    test.identical( got, true );
+
+    test.case = 'src - vector without zeros, onEach - returns src.length';
+    var src = _.vectorAdapter.fromNumber( 7, 6 );
+    var got = _.vectorAdapter.all( src, ( e, k, s ) => s.length );
+    test.identical( got, true );
+
+    test.case = 'src - vector with zeros, onEach - returns src.length';
+    var src = _.vectorAdapter.fromNumber( 0, 6 );
+    var got = _.vectorAdapter.all( src, ( e, k, s ) => s.length );
+    test.identical( got, true );
+
+    /* */
+
+    test.case = 'src - empty vector, onEach - returns undefined';
+    var src = _.vectorAdapter.fromNumber( 7, 0 );
+    var got = _.vectorAdapter.all( src, ( e, k, s ) => undefined );
+    test.identical( got, true );
+
+    test.case = 'src - vector without zeros, onEach - returns undefined';
+    var src = _.vectorAdapter.fromNumber( 7, 6 );
+    var got = _.vectorAdapter.all( src, ( e, k, s ) => undefined );
+    test.identical( got, undefined );
+
+    test.case = 'src - vector with zeros, onEach - returns undefined';
+    var src = _.vectorAdapter.fromNumber( 0, 6 );
+    var got = _.vectorAdapter.all( src, ( e, k, s ) => undefined );
+    test.identical( got, undefined );
+  }
+}
+
+//
+
 function _allIdentical( test, r, t, array )
 {
   var f = !t;
@@ -14234,6 +14375,7 @@ var Self =
     allRoutineFromLong,
     allRoutineFromLongLrangeAndStride,
     allRoutineFromNumberWithVectorAdapter,
+    allRoutineFromNumberWithNumber,
 
     allIdentical,
     allNotIdentical,
