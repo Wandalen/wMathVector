01e4a9ab1a0989fa5c8c1d8d6922e393d8629029
Dmytro <dm.vict.kr@gmail.com>
2020-03-31 18:15:56 +0300

    Fix samples

--- sample/Sample2.js
+++ sample/Sample2.js
@@ -8,9 +8,11 @@ var _ = wTools;
 var a1 = [ 1, 2, 3 ];
 var a2 = [ 1, 2, 3 ];
 var a3 = [ 1, 2, 3 ];
-var avector = wTools.avector.add( a1, a2, a3 );
+var avector = _.avector.add( a1, a2, a3 );
 console.log( 'avector: ', avector );
+/* log "avector: [ 3, 6, 9 ]" */
 console.log( 'a1:', a1 );
+/* log "a1: [ 3, 6, 9 ]" */
 
 /* add 2 avectors and scalar */
 
@@ -18,9 +20,11 @@ var a1 = [ 0, 0, 0 ];
 var a2 = [ 0, 0, 0 ];
 var a3 =  5;
 var a4 =  7;
-var avector = wTools.avector.add( a1, a2, a3, a4 );
+var avector = _.avector.add( a1, a2, a3, a4 );
 console.log( 'avector: ', avector );
+/* log "avector:  [ 12, 12, 12 ]" */
 console.log( 'a1:', a1 );
+/* log "a1: [ 12, 12, 12 ]" */
 
 /* saving add result to new avector */
 
@@ -28,9 +32,11 @@ var a1 = [ 0, 0, 0 ];
 var a2 = [ 0, 0, 0 ];
 var a3 =  5;
 var a4 =  7;
-var vector = wTools.avector.add( null, a1, a2, a3, a4 );
+var vector = _.avector.add( null, a1, a2, a3, a4 );
 console.log( 'avector: ', avector );
+/* log "avector:  [ 12, 12, 12 ]" */
 console.log( 'a1:', a1 );
+/* log "a1: [ 0, 0, 0 ]" */
 
 /* using vector wrapper and single avector as container */
 
@@ -38,27 +44,42 @@ var a = [ 0, 0, 1, 1, 2, 2 ];
 
 var v1Offset = 0;
 var v1Length = 3;
-var v1 = wTools.vector.fromSubArray( a, v1Offset, v1Length );
+var v1 = _.vectorAdapter.fromLongLrange( a, v1Offset, v1Length );
 
 var v2Offset = v1Length;
 var v2Length = 3;
-var v2 = wTools.vector.fromSubArray( a, v2Offset, v2Length );
+var v2 = _.vectorAdapter.fromLongLrange( a, v2Offset, v2Length );
 
-wTools.vector.add( v1, v2 );
+_.avector.add( v1, v2 );
 
 console.log( 'v1: ', v1 );
+/* log
+"v1:  VectorAdapterFromLongShrinked {
+  _vectorBuffer: [ 1, 2, 3, 1, 2, 2 ],
+  length: 3,
+  offset: 0
+}" */
 console.log( 'v2: ', v2 );
+/* log
+"v2:  VectorAdapterFromLongShrinked {
+  _vectorBuffer: [ 1, 2, 3, 1, 2, 2 ],
+  length: 3,
+  offset: 3
+}" */
 console.log( 'a: ', a );
+/* log "a:  [ 1, 2, 3, 1, 2, 2 ]" */
 
 /* vector wrapper from avector */
 
 var avector = [ 1, 2, 3 ];
-var vector = wTools.vector.from( avector );
+var vector = _.vectorAdapter.from( avector );
 console.log( vector )
+/* log "VectorAdapterFromLong { _vectorBuffer: [ 1, 2, 3 ] }" */
 
 /* setting and getting data from container through vector wrapper */
 
 var avector = [ 1, 2, 3 ];
-var vector = wTools.vector.from( avector );
+var vector = _.vectorAdapter.from( avector );
 vector.eSet( 0, 5 );
-console.log( vector.eGet( 0 ) )
+console.log( vector.eGet( 0 ) );
+/* log 5 */
--- sample/SampleAll.js
+++ sample/SampleAll.js
@@ -3,40 +3,45 @@ require( 'wmathvector' );
 
 var _ = wTools;
 
-debugger;
-
 function onEvaluate( src )
 {
   return - 5 < src && src < 0 ; // numbers in range
 }
 
-var v1 = _.vector.from( [ -1, -1.5, -2 ] );
-var bool = _.vector.all( v1, onEvaluate );
+var v1 = _.vectorAdapter.from( [ -1, -1.5, -2 ] );
+var bool = _.vectorAdapter.all( v1, onEvaluate );
 logger.log('ALL true -', bool );
+/* log "ALL true - true" */
 
-var v1 = _.vector.from( [ 0, 1, -2 ]);
-var bool = _.vector.all( v1, onEvaluate );
+var v1 = _.vectorAdapter.from( [ 0, 1, -2 ]);
+var bool = _.vectorAdapter.all( v1, onEvaluate );
 logger.log('ALL false -', bool );
-debugger;
+/* log "ALL false - false" */
 
-var v1 = _.vector.from( [ -1, 1, 2 ]);
-var bool = _.vector.any( v1, onEvaluate );
+var v1 = _.vectorAdapter.from( [ -1, 1, 2 ]);
+var bool = _.vectorAdapter.any( v1, onEvaluate );
 logger.log('ANY true -', bool );
+/* log "ANY true - true" */
 
-var v1 = _.vector.from( [ 0, 1, 2 ]);
-var bool = _.vector.all( v1, onEvaluate );
+var v1 = _.vectorAdapter.from( [ 0, 1, 2 ]);
+var bool = _.vectorAdapter.all( v1, onEvaluate );
 logger.log('ANY false -', bool );
+/* log "ANY false - false" */
 
-var v1 = _.vector.from( [ 1, 1.5, 2 ]);
-var bool = _.vector.none( v1, onEvaluate );
+var v1 = _.vectorAdapter.from( [ 1, 1.5, 2 ]);
+var bool = _.vectorAdapter.none( v1, onEvaluate );
 logger.log('NONE true -', bool );
+/* log "NONE true - true" */
 
-var v1 = _.vector.from( [ 0, 1, -2 ]);
-var bool = _.vector.none( v1, onEvaluate );
+var v1 = _.vectorAdapter.from( [ 0, 1, -2 ]);
+var bool = _.vectorAdapter.none( v1, onEvaluate );
 logger.log('NONE false -', bool );
+/* log "NONE false - false" */
 
-var v1 = _.vector.from( [ ]);
-var bool = _.vector.is( v1, onEvaluate );
-logger.log('Is -', bool, v1 );
+var v1 = _.vectorAdapter.from( [ 0, 1, 2 ] );
+var v2 = _.vectorAdapter.from( [ 0, 4, 8 ] );
+logger.log( 'AREPARALLEL true - ', _.vectorAdapter.areParallel( v1, v2, 0.005 ) );
 
-logger.log( _.vector.areParallel( _.vector.from( [ 0, 1, 2 ] ), _.vector.from( [ 0, 4, 8 ] ), 0.005 ) );
+var v1 = _.vectorAdapter.from( [ 0, 1, 2 ] );
+var v2 = _.vectorAdapter.from( [ 0, 2, 8 ] );
+logger.log( 'AREPARALLEL false - ', _.vectorAdapter.areParallel( v1, v2, 0.005 ) );
