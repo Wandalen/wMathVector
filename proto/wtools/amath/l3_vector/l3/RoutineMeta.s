f14e0ed85c42dcc1ad787aa5d399c6de1b9c1cf5
wandalen <wandalen.me@gmail.com>
2020-12-03 16:11:55 +0200

    .

--- proto/wtools/amath/l3_vector/l3/RoutineMeta.s
+++ proto/wtools/amath/l3_vector/l3/RoutineMeta.s
@@ -43,8 +43,8 @@ function _routinePostForm( theRoutine, routineName )
 
   /* adjust */
 
-  _.mapSupplement( op, _.vectorAdapter.OperationDescriptor2.Fields );
-  _.assertMapHasOnly( op, _.vectorAdapter.OperationDescriptor2.Fields );
+  _.mapSupplement( op, _.vectorAdapter.OperationDescriptor2.Props );
+  _.assertMapHasOnly( op, _.vectorAdapter.OperationDescriptor2.Props );
 
   op.returningPrimitive = !!op.returningPrimitive;
   op.returningLong = !!op.returningLong;
@@ -1140,7 +1140,7 @@ function _routineForOperation_functor( dop )
 
 _routineForOperation_functor.defaults =
 {
-  ... OperationDescriptor1.Fields,
+  ... OperationDescriptor1.Props,
   onScalar_functor : null,
   onVectors_functor : null,
 }
@@ -2276,7 +2276,7 @@ function __operationReduceToScalar_functor( operation )
 
 __operationReduceToScalar_functor.defaults =
 {
-  ... OperationDescriptor1.Fields,
+  ... OperationDescriptor1.Props,
 
   onScalar : null,
   onVectorsBegin : null,
